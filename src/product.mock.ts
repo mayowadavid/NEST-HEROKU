cca1e5e8c3608c14dd971d233c494dd72e86147b
mayowadavid <mayowadavid100@gmail.com>
2022-05-18 16:14:28 +0100

    first commit

---
+++ src/product.mock.ts
@@ -0,0 +1,20 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: 'First product',
+    description: 'this is the description for the first product',
+    price: '100',
+  },
+  {
+    id: 2,
+    name: 'second product',
+    description: 'this is the description for the first product',
+    price: '200',
+  },
+  {
+    id: 3,
+    name: 'third product',
+    description: 'this is the description for the first product',
+    price: '300',
+  },
+];
