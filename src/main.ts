322118f5c8551386e0418a2f52b3ba727bfc0caa
mayowadavid <mayowadavid100@gmail.com>
2022-05-18 16:48:08 +0100

    initials

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000); // update this line
 }
 bootstrap();
